45e23dc4d16493f9b07988f34b33b99f938e89f1
bwesterg <benton.westergaard@gmail.com>
2023-11-04 13:32:35 -0600

    adds simple JS on-off button to home page to test console problems. This does not use the more efficient setState functionality of React b/c I'm trying to fix some environment problems.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,18 @@ import Navbar from '../components/Navbar';
 
 
 export default function Home() {
+    
+    const handleClick = () => {
+        let currentValue = document.getElementById("my-button").innerText;
+        if(currentValue == "Off"){
+            console.log("button is on");
+            document.getElementById("my-button").innerText = "On";
+        } else {
+            console.log("button is off");
+            document.getElementById("my-button").innerText = "Off";
+        }
+    }
+
     return (
         <>
             <Navbar />
@@ -12,6 +24,10 @@ export default function Home() {
             </div>
             <div className="paragraph-div">
                 <p>this is some stuff on my home page</p>
+                <div id="on-off-playground">
+                    <button id="my-button" onClick={handleClick}>On</button>
+                </div>
+                
             </div>
         </>
     )
